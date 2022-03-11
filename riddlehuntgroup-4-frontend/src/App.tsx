aa8ad96d4652a0c6f5dbd33e262341e83a3d22b9
jens harder <jens180980@gmail.com>
2022-03-11 11:07:30 +0100

    frontend boilerplate ready

---
+++ riddlehuntgroup-4-frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import { IonApp, setupIonicReact } from "@ionic/react";
+
+/* Core CSS required for Ionic components to work properly */
+import "@ionic/react/css/core.css";
+
+/* Basic CSS for apps built with Ionic */
+import "@ionic/react/css/normalize.css";
+import "@ionic/react/css/structure.css";
+import "@ionic/react/css/typography.css";
+
+setupIonicReact();
+
+const App: React.FC = () => <IonApp></IonApp>;
+
+export default App;
