9a211b95d0c0ffe67c2d88cfc225e8d72f9b5c48
jens harder <jens180980@gmail.com>
2022-03-16 11:42:10 +0100

    fixed picture link

--- riddlehuntgroup-4-frontend/src/components/Pages/Home/Home.tsx
+++ riddlehuntgroup-4-frontend/src/components/Pages/Home/Home.tsx
@@ -1,7 +1,14 @@
+import Cards from "../../Partials/Cards/Cards";
+
 const Home: React.FC = () => {
   return (
     <>
-      <h1>Hej</h1>
+      <Cards
+        src="./testpic.jpg"
+        subtitle="Restaurant"
+        title="BioLoco"
+        description="100% vegan food, and you can even bring your pet"
+      />
     </>
   );
 };
