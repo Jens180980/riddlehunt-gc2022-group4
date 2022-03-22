07517746bdb6256f1fb7285c7cc61d6e05eeb722
Lyks012 <kaali20021@gmail.com>
2022-03-22 10:40:06 +0000

    using env variables

--- riddlehuntgroup-4-backend/Config/db.config.js
+++ riddlehuntgroup-4-backend/Config/db.config.js
@@ -1,9 +1,9 @@
 require("dotenv").config();
 module.exports = {
-  HOST: "localhost",
-  USER: "root",
-  PASSWORD: "",
-  DB: "riddlehunt",
+  HOST: process.env.MYSQL_ADDON_HOST,
+  USER: process.env.MYSQL_ADDON_USER,
+  PASSWORD: process.env.MYSQL_ADDON_PASSWORD,
+  DB: process.env.MYSQL_ADDON_DB,
   dialect: "mysql",
   pool: {
     max: 5,
@@ -11,16 +11,4 @@ module.exports = {
     acquire: 30000,
     idle: 10000,
   },
-
-  // HOST: process.env.MYSQL_ADDON_HOST,
-  // USER: process.env.MYSQL_ADDON_USER,
-  // PASSWORD: process.env.MYSQL_ADDON_PASSWORD,
-  // DB: process.env.MYSQL_ADDON_DB,
-  // dialect: "mysql",
-  // pool: {
-  //   max: 5,
-  //   min: 0,
-  //   acquire: 30000,
-  //   idle: 10000,
-  // },
 };
