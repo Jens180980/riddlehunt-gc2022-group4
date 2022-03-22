ba90f1cc1299a6d10605a51c4f8fd31aa3af2408
Lyks012 <kaali20021@gmail.com>
2022-03-22 11:42:57 +0000

    fixing react dependencies

--- frontend/capacitor.config.ts
+++ frontend/capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'riddlehuntgroup4-frontend',
+  appName: 'frontend',
   webDir: 'build',
   bundledWebRuntime: false
 };
