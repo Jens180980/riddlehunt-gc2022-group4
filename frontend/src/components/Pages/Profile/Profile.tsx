69c547e142f200a6c40ccd25fbbeb8d929d326f5
Nairtoch <nairtoch@gmail.com>
2022-03-24 10:41:19 +0000

    ProfileTest

--- frontend/src/components/Pages/Profile/Profile.tsx
+++ frontend/src/components/Pages/Profile/Profile.tsx
@@ -1,41 +1,54 @@
-import { closeOutline } from "ionicons/icons";
+import { leaf, locate, logOut, person, settings } from "ionicons/icons";
 import React from 'react';
-import { IonList, IonItem, IonLabel, IonContent, IonAvatar, IonIcon } from '@ionic/react';
+import { IonList, IonItem, IonLabel, IonContent, IonAvatar, IonIcon, IonRefresher, IonRefresherContent } from '@ionic/react';
+import "./Profile.css";
+import Header from "../../Partials/Header/Header";
 
 
 export const Profile: React.FC = () => {
   return (
     <>
       <IonContent>
-        {/*-- List of Text Items --*/}
-        <IonList>
+      {/* <Header title="Riddlehunt 4.0" /> */}
+
+        <div id="ProfileCircle"></div>
+        <section id="ProfileName">
+          <img src="https://i.ibb.co/ZJvLSBD/logo1.png" alt="logo, temporary picture" />
+          <h3>Username</h3>
+        </section>
+
+        <IonList id="ProfileList">
           <IonItem>
             <IonAvatar slot="start">
-              {<IonIcon icon="person"></IonIcon>}
+              <IonIcon icon={person} size="large"></IonIcon>
             </IonAvatar>
             <IonLabel>My Profile</IonLabel>
           </IonItem>
+
           <IonItem>
             <IonAvatar slot="start">
-              <img src="https://i.ibb.co/7WrTKnT/efa358df0d83.jpg" />
+              <IonIcon icon={locate} size="large"></IonIcon>
             </IonAvatar>
             <IonLabel>Location</IonLabel>
           </IonItem>
+
           <IonItem>
             <IonAvatar slot="start">
-              <img src="https://i.ibb.co/7WrTKnT/efa358df0d83.jpg" />
+              <IonIcon icon={leaf} size="large"></IonIcon>
             </IonAvatar>
             <IonLabel>My Places</IonLabel>
           </IonItem>
+
           <IonItem>
             <IonAvatar slot="start">
-              <img src="https://i.ibb.co/7WrTKnT/efa358df0d83.jpg" />
+              <IonIcon icon={settings} size="large"></IonIcon>
             </IonAvatar>
             <IonLabel>Settings</IonLabel>
           </IonItem>
+
           <IonItem>
             <IonAvatar slot="start">
-              <img src="https://i.ibb.co/7WrTKnT/efa358df0d83.jpg" />
+              <IonIcon icon={logOut} size="large"></IonIcon>
             </IonAvatar>
             <IonLabel>Log Out</IonLabel>
           </IonItem>
