ed857ce9d2e951758b5a83dd10e209d4c9ebde14
jens harder <jens180980@gmail.com>
2022-03-24 09:34:07 +0100

    cleanup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,10 @@
 // Ionic components import
-import {
-  IonApp,
-  setupIonicReact,
-  IonTabs,
-  IonTabBar,
-  IonTabButton,
-  IonIcon,
-  IonLabel,
-  IonRouterOutlet,
-} from "@ionic/react";
+import { IonApp, setupIonicReact } from "@ionic/react";
 import { useState, createContext, useEffect } from "react";
 import Fetch from "./Services/places.service";
 
 // Pages import
 import Navigation from "./components/Partials/Navigation/Navigation";
-import Home from "./components/Pages/Home/Home";
 
 /* Core CSS required for Ionic components to work properly */
 import "@ionic/react/css/core.css";
@@ -26,15 +16,13 @@ import "@ionic/react/css/typography.css";
 import PlaceService from "./Services/places.service";
 
 setupIonicReact();
-interface contextInterface {
-  placesData: any;
-  setPlacesData: any;
-}
+
 export const placesContext = createContext<any>({});
 
 const App: React.FC = () => {
   const placeService = new PlaceService();
   const [placesData, setPlacesData] = useState();
+
   useEffect(() => {
     const getData = async () => {
       placeService.getPlaces().then((apiPlaces) => {
